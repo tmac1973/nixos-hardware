072391da1d2e987f85dcc27fa149d034725b4731
Timothy MacDonald <tbaxmacdonald@gmail.com>
2025-06-17 07:21:55 -0500

    Update audio.nix

--- minisforum/v3/audio.nix
+++ minisforum/v3/audio.nix
@@ -2,9 +2,9 @@
 {
   # Fix microphone.
   # Based on https://github.com/mudkipme/awesome-minisforum-v3/issues/10#issuecomment-2317474057 (Volume control workaround doesn't work on Arch) 
-  boot.extraModprobeConfig = ''
-    options snd-hda-intel model=alc256-asus-aio
-  '';
+  #boot.extraModprobeConfig = ''
+    #options snd-hda-intel model=alc256-asus-aio
+  #'';
 
   # Based on https://github.com/mudkipme/awesome-minisforum-v3/issues/9#issue-2407782714 (volume control fixes for arch)
   services.pipewire.wireplumber.extraConfig."alsa-soft-mixer"."monitor.alsa.rules" = [
@@ -32,15 +32,15 @@
       # Disable audio session suspension.
       # Fix bug with plugged in headphones.
       # Based on https://github.com/mudkipme/awesome-minisforum-v3?tab=readme-ov-file#disable-audio-session-suspension (Disable audio session suspension)
-      actions.update-props."session.suspend-timeout-seconds" = "0";
-      matches = [
-        {
+      #actions.update-props."session.suspend-timeout-seconds" = "0";
+      #matches = [
+      #  {
           "node.name" = "~alsa_input.*";
-        }
-        {
-          "node.name" = "~alsa_output.*";
-        }
-      ];
+       # }
+      #  {
+      #    "node.name" = "~alsa_output.*";
+      #  }
+      #];
     }
   ];
 
